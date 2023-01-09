a01bbfadcaeab310d70d38196a95913b7599389d
Victoria Vinogradova <v.vinogradova@swooapp.com>
2023-01-09 15:16:30 +0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Виктория Виноградова on 09.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
