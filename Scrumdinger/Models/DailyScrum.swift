b3f2edc44b4c16657b30ec17c748e42edf8e85e3
Victoria Vinogradova <vinovinka@yandex.ru>
2023-01-16 23:08:02 +0700

    Added detailEditView

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -26,6 +26,17 @@ extension DailyScrum {
             self.name = name
         }
     }
+
+    struct Data {
+        var title: String = ""
+        var attendees: [Attendee] = []
+        var lengthInMinutes: Double = 5
+        var theme: Theme = .seafoam
+    }
+
+    var data: Data {
+        Data(title: title, attendees: attendees, lengthInMinutes: Double(lengthInMinutes), theme: theme)
+    }
 }
 
 extension DailyScrum {
--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -11,6 +11,8 @@ struct DetailView: View {
 
     let scrum: DailyScrum
 
+    @State private var isPresentingEditView = false
+
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
@@ -43,6 +45,29 @@ struct DetailView: View {
             }
         }
         .navigationTitle("Design")
+        .toolbar {
+            Button("Edit") {
+                isPresentingEditView = true
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationView {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
 
